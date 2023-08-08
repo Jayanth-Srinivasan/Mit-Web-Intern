8c013b3a9e38cd3587d1b5c6b6d3c388824102cb
Jayanth Srinivasan <jayanthsrinivasan2003@gmail.com>
2023-08-08 19:58:39 +0530

    routing added

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Hero from '../../assets/Hero.svg';
 import './Landing.css';
+import { useNavigate } from 'react-router-dom';
 
 function Landing() {
+  const navigate = useNavigate();
   return (
     <div className="hero">
       <div className='heroCard'>
@@ -10,7 +12,7 @@ function Landing() {
           <div className='box'>
             <h1 className='heading'>Everything You Need is Here</h1>
             <p className='desc'>Get started to Start Shopping blah blah blah</p>
-            <button className='cta'>Get Started</button>
+            <button onClick={() => navigate('/home')} className='cta'>Get Started</button>
           </div>
         </div>
         <div className='imageBox'>

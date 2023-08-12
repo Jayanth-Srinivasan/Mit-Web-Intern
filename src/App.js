7d4474752350ea2863dc73fbe44253f9a62c4b51
Jayanth Srinivasan <jayanthsrinivasan2003@gmail.com>
2023-08-12 19:38:44 +0530

    User data added to DB

--- src/App.js
+++ src/App.js
@@ -2,28 +2,71 @@ import './App.css';
 import { Routes, Route,useNavigate } from 'react-router-dom';
 import Landing from './components/Landing/Landing';
 import Home from './components/Home/Home';
-import {GoogleAuthProvider,signInWithPopup} from 'firebase/auth';
-import {auth} from './firebase'
+import {GoogleAuthProvider,signInWithPopup, signOut} from 'firebase/auth';
+import {auth, db} from './firebase'
+import { useState } from 'react';
+import { doc, setDoc } from 'firebase/firestore';
 
 
 function App() {
+  const [user,setUser] = useState(JSON.parse(localStorage.getItem('user')));
   const googleProvider = new GoogleAuthProvider();
   const navigate = useNavigate();
   
 
   const SignIn = () => {
     signInWithPopup(auth,googleProvider)
-    .then((a) => {
-      console.log("Logged in");
-      console.log(a);
-      navigate('/home');
+    .then(async(a) => {
+      setDoc(
+        doc(db,"users",a.user.uid),
+        {
+          name: a.user.displayName,
+          email: a.user.email,
+          uid: a.user.uid,
+          photo: a.user.photoURL,
+        },
+        {merge: true}
+      ).then(async() => {
+        navigate('/home');
+        localStorage.setItem(
+          "user",
+          JSON.stringify({
+            name: a.user.displayName,
+            email: a.user.email,
+            uid: a.user.uid,
+            photo: a.user.photoURL,
+          })
+        );
+        setUser({
+            name: a.user.displayName,
+            email: a.user.email,
+            uid: a.user.uid,
+            photo: a.user.photoURL,
+        })
+
+      })
+      
+    })
+    .catch((err) => console.log(err));
+  }
+
+  const onSignOut = () => {
+    signOut(auth)
+    .then(()=>{
+      setUser(null);
+      localStorage.removeItem("user");
+      navigate('/');
     })
     .catch((err) => console.log(err));
   }
   return (
     <Routes>
-      <Route path='/' element={<Landing SignIn={SignIn} />} />
-      <Route path='/home' element={<Home/>}/>
+      {
+        !user?
+        <Route path='/' element={<Landing SignIn={SignIn} />} />
+        :
+        <Route path='/home' element={<Home onSignOut={onSignOut} user={user}/>}/>
+      }
     </Routes>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import "./Home.css"
 
-function Home() {
+function Home({onSignOut,user}) {
   return (
     <header className='navBar'>
         <h1 className='logo-text'>Mit-Kart</h1>
         <div className='profile'>
-        <div>Name</div>
-        <div>Sign Out</div>
+        <div>{user.name}</div>
+        <div onClick={onSignOut} className='sign-out'>Sign Out</div>
         </div>
     </header>
   )
